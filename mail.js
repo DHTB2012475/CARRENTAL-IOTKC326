ca61d8d9c5f6c721d72f84bbb659de5c4d4bb203
KC326-IOT <116433510+DHTB2012475@users.noreply.github.com>
2023-02-14 14:18:47 +0700

    u2

--- mail.js
+++ mail.js
@@ -1,22 +1,22 @@
 function sendMail() {
-    var params = {
-      name: document.getElementById("room1Name").value,
-      email: document.getElementById("room1Email").value,
-      message: document.getElementById("room1Phone").value,
-    };
-  
-    const serviceID = "service_57kthaa";
-    const templateID = "template_sa95mei";
-  
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("room1Name").value = "";
-          document.getElementById("room1Email").value = "";
-          document.getElementById("room1Phone").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
-  
-      })
-      .catch(err=>console.log(err));
-  
-  }
+  var params = {
+    name: document.getElementById("room1Name").value,
+    email: document.getElementById("room1Email").value,
+    message: document.getElementById("room1Phone").value,
+  };
+
+  const serviceID = "service_vv68pg3";
+  const templateID = "template_cjuk3j8";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("room1Name").value = "";
+        document.getElementById("room1Email").value = "";
+        document.getElementById("room1Phone").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
