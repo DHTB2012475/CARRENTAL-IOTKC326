8434112c3385535a596ecaa89284b76bf68c9717
KC326-IOT <116433510+DHTB2012475@users.noreply.github.com>
2023-02-15 00:13:41 +0700

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,18 +1,47 @@
 function sendMail() {
   var params = {
-    name: document.getElementById("room1Name").value,
+    khachhang: document.getElementById("room1Name").value,
     email: document.getElementById("room1Email").value,
-    message: document.getElementById("room1Phone").value,
+    sodienthoai: document.getElementById("room1Phone").value,
+    loaixe: document.getElementById("room1type").value,
+    giathuengay: document.getElementById("room1Price").value,
+    ngaythue: document.getElementById("room1day").value,
+    songaythue: document.getElementById("number1day").value,
+    tongthanhtoan: document.getElementById("room1Phone").value,
+    
+
+
+
+//     {{khachhang}}
+
+// {{email}}
+
+// {{sodienthoai}}
+
+// {{loaixe}}
+
+// {{giathue/ngay}}
+
+// {{ngaythue}}
+
+// {{songaythue}}
+
+// {{tongthanhtoan}}
   };
 
   const serviceID = "service_vv68pg3";
-  const templateID = "template_cjuk3j8";
+  const templateID = "template_46pj6pd";
 
     emailjs.send(serviceID, templateID, params)
     .then(res=>{
         document.getElementById("room1Name").value = "";
         document.getElementById("room1Email").value = "";
         document.getElementById("room1Phone").value = "";
+        document.getElementById("room1type").value= "";
+    document.getElementById("room1Price").value= "";
+document.getElementById("room1day").value= "";
+document.getElementById("number1day").value= "";
+document.getElementById("room1Phone").value= "";
         console.log(res);
         alert("Your message sent successfully!!")
 
