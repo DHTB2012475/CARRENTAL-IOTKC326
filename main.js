c7eb95c76928757940a1b716413a73d414cbf716
KC326-IOT <116433510+DHTB2012475@users.noreply.github.com>
2023-02-15 23:54:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -304,6 +304,13 @@ document.getElementById('formcancel').addEventListener('submit', (e)=>{
     })
 });
 
-    
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey){
+       event.preventDefault();
+    }
+    if(event.keyCode == 123){
+       event.preventDefault();
+    }
+}); 
     
        
